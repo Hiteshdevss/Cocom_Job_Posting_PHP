26c49032d462dd6bee2be7585020ba09fbe64bc7
Hitesh Bhagwat <155135951+Hiteshdevss@users.noreply.github.com>
2024-05-18 23:24:47 +0530

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php require './components/_navbar.php'?>
-<?php require './home.php'?>
-<?php require './components/_footer.php'?>
-<script src="//code.tidio.co/iiifjimfg5spwyx0ujxk9euq63ylit2n.js" async></script>
+<?php
+
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cocom | Loading........</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="h-screen flex justify-center items-center">
+  <img src="./assets/liquid-loading.gif" alt="loading">
+</div>
+<script>
+        setTimeout(function() {
+            window.location.href = "home.php";
+        }, 3000); // Redirect after 5 seconds (5000 milliseconds)
+</script>
+</body>
+</html>';
+?>
